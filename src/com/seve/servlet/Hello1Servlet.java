3d446c8e0994e3b8f464e5300dd246bebb983e86
1070841863 <1070841863@qq.com>
2019-03-23 10:14:12 +0800

    test备注

---
+++ src/com/seve/servlet/Hello1Servlet.java
@@ -0,0 +1,21 @@
+package com.seve.servlet;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet(name = "Hello1Servlet")
+public class Hello1Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+        response.getWriter().println("<h1>Hello Servlet!</h1>");
+        response.getWriter().println(new Date().toString());
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+        doPost(request,response);
+    }
+}
